d2715b475eaa59b3fb0cfd2b9b16b54883dbc8b8
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-15 14:54:51 -0300

    feat: snakeJS moveSnake

--- js/snake.js
+++ js/snake.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import { ctx } from "./selectElement";
-import {size} from './variable';
+import { size, direction } from "./variable";
 
 const initialPosition = { x: 270, y: 240 }; // posição inicial da snake
 
@@ -11,7 +11,8 @@ export const drawSnake = () => {
   ctx.fillStyle = "#ddd"; // cor da snake
 
   snake.forEach((position, idx) => {
-    if (idx == snake.length - 1) { // se for a ultima posição (a cabeça da snake) - aplica a cor branca;
+    if (idx == snake.length - 1) {
+      // se for a ultima posição (a cabeça da snake) - aplica a cor branca;
       ctx.fillStyle = "white";
     }
 
@@ -19,3 +20,21 @@ export const drawSnake = () => {
   });
 };
 
+export const moveSnake = () => {
+  const head = snake[snake.length - 1];
+
+  if (direction == "right") {
+    snake.push({x: head.x + size, y: head.y});
+  }
+  if (direction == "left") {
+    snake.push({x: head.x - size, y: head.y});
+  }
+  if (direction == "down") {
+    snake.push({x: head.x, y: head.y + size});
+  }
+  if (direction == "up") {
+    snake.push({x: head.x, y: head.y - size});
+  }
+
+  snake.shift();
+};
