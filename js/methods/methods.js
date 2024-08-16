4246c76895801e2bb1cec342f87429eb0243422a
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-16 17:15:32 -0300

    feat: snakeJS gameOver

--- js/methods/methods.js
+++ js/methods/methods.js
@@ -1,7 +1,8 @@
-import { audio, canvas, score } from '../selectElements/selectElement.js';
+import { audio, canvas, finalScore, menu, score } from '../selectElements/selectElement.js';
 import { size } from '../variables/variable.js';
 import { snake } from '../snake/snake.js';
 import { food } from '../food/food.js';
+import { direction } from '../main.js';
 
 export const randomNumber = (min, max) => {
   return Math.round(Math.random() * (max - min) + min);
@@ -46,15 +47,23 @@ export const chackEat = () => {
   }
 };
 
-export const checkCollision = () => {
-  const head = snake[snake.length - 1];
-  const canvasLimit = canvas.width - size;
-  const neckIndex = snake.length - 2;
+export const gameOver = () => {
+  direction = undefined; // parar a movimentação
+  menu.style.display = 'flex'; // mostra o menu
+  finalScore.textContent = score.textContent; // add o score atual ao score final
+  canvas.style.filter = 'blur(2px)'; // desfoca a o fundo do game
+};
 
-  const wallCollision =
+export const checkCollision = () => {
+  const head = snake[snake.length - 1]; // posição da cabeça
+  const canvasLimit = canvas.width - size; // limite que a snake pode ir
+  const neckIndex = snake.length - 2; // posição do pescoço
+
+  const wallCollision = // bateu na perede?
     head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit;
 
   const selfCollision = snake.find((position, idx) => {
+    // bateu em si mesma? se sim retorna um objeto que sera convertido em boolean no if
     return idx < neckIndex && position.x == head.x && position.y == head.y;
   });
 
