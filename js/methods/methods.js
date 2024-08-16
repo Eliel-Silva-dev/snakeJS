be5679336c9391f49194b06dd1cad49acf305b12
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-16 17:05:02 -0300

    feat: snakeJS checkCollision

--- js/methods/methods.js
+++ js/methods/methods.js
@@ -1,18 +1,18 @@
 import { audio, canvas, score } from '../selectElements/selectElement.js';
 import { size } from '../variables/variable.js';
-import {snake} from '../snake/snake.js';
+import { snake } from '../snake/snake.js';
 import { food } from '../food/food.js';
 
-const randomNumber = (min, max) => {
+export const randomNumber = (min, max) => {
   return Math.round(Math.random() * (max - min) + min);
 };
 
-const randomPosition = () => {
+export const randomPosition = () => {
   const number = randomNumber(0, canvas.width - size);
   return Math.round(number / 30) * 30;
 };
 
-const randomColor = () => {
+export const randomColor = () => {
   const red = randomNumber(0, 255);
   const green = randomNumber(0, 255);
   const blue = randomNumber(0, 255);
@@ -27,7 +27,7 @@ export const incrementScore = () => {
 export const chackEat = () => {
   const head = snake[snake.length - 1];
 
-  if(head.x == food.x && head.y == food.y) {
+  if (head.x == food.x && head.y == food.y) {
     incrementScore();
     snake.push(head);
     audio.play;
@@ -35,7 +35,7 @@ export const chackEat = () => {
     let x = randomPosition();
     let y = randomPosition();
 
-    while(snake.find((position) => position.x == x && position.y == y)) {
+    while (snake.find((position) => position.x == x && position.y == y)) {
       x = randomPosition();
       y = randomPosition();
     }
@@ -46,4 +46,19 @@ export const chackEat = () => {
   }
 };
 
-export { randomNumber, randomPosition, randomColor };
+export const checkCollision = () => {
+  const head = snake[snake.length - 1];
+  const canvasLimit = canvas.width - size;
+  const neckIndex = snake.length - 2;
+
+  const wallCollision =
+    head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit;
+
+  const selfCollision = snake.find((position, idx) => {
+    return idx < neckIndex && position.x == head.x && position.y == head.y;
+  });
+
+  if (wallCollision || selfCollision) {
+    gameOver();
+  }
+};
