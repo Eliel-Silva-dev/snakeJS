7669128f4e028e1d5172190bbeec84eb2836b864
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-08-24 19:56:18 -0300

    fix: react snake move func collision

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,6 +107,82 @@ export default function Home() {
   };
   /*fim snake*/
 
+  export const incrementScore = () => {
+    score.textContent = `${+(score?.textContent as string) + 10}`;
+  };
+
+  type TMoveKey = {
+    key: string;
+  };
+
+  export const moveKey = ({ key }: TMoveKey) => {
+    if (key == 'ArrowUp' && direction != 'down') {
+      setDirection('up');
+    }
+    if (key == 'ArrowDown' && direction != 'up') {
+      setDirection('down');
+    }
+    if (key == 'ArrowRight' && direction != 'left') {
+      setDirection('right');
+    }
+    if (key == 'ArrowLeft' && direction != 'right') {
+      setDirection('left');
+    }
+  };
+
+  export const chackEat = () => {
+    const head = snake[snake.length - 1];
+
+    if (head.x == food.x && head.y == food.y) {
+      incrementScore();
+      snake.push(head);
+      audio.play();
+
+      let x = randomPosition();
+      let y = randomPosition();
+
+      while (snake.find((position) => position.x == x && position.y == y)) {
+        x = randomPosition();
+        y = randomPosition();
+      }
+
+      food.x = x;
+      food.y = y;
+      food.color = randomColor();
+    }
+  };
+
+  /*inicio gameover */
+  const gameOver = () => {
+    setDirection(''); // parar a movimentação
+    menu.style.display = 'flex'; // mostra o menu
+    finalScore.textContent = score.textContent; // add o score atual ao score final
+    canvas.style.filter = 'blur(2px)'; // desfoca a o fundo do game
+
+    document.removeEventListener('keydown', moveKey);
+  };
+  /*fim gameover */
+
+  /*inicio collision */
+  const checkCollision = () => {
+    const head = snake[snake.length - 1]; // posição da cabeça
+    const canvasLimit = canvas.width - size; // limite que a snake pode ir
+    const neckIndex = snake.length - 2; // posição do pescoço
+
+    const wallCollision = // bateu na perede?
+      head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit;
+
+    const selfCollision = snake.find((position, idx) => {
+      // bateu em si mesma? se sim retorna um objeto que sera convertido em boolean no if
+      return idx < neckIndex && position.x == head.x && position.y == head.y;
+    });
+
+    if (wallCollision || selfCollision) {
+      gameOver();
+    }
+  };
+  /*fim collision*/
+
   const gameloop = () => {
     clearTimeout(loopId);
 
--- src/shared/func/methods.ts
+++ src/shared/func/methods.ts
@@ -1,7 +1,4 @@
-import { audio, canvas, finalScore, menu, score } from './selectElement';
-import { direction, setDirection, size } from './variable';
-import { food } from './food';
-import { snake } from '@/app/page';
+import { size } from './variable';
 
 export const randomNumber = (min: number, max: number) => {
   return Math.round(Math.random() * (max - min) + min);
@@ -18,76 +15,4 @@ export const randomColor = () => {
   const blue = randomNumber(0, 255);
 
   return `rgb(${red}, ${green}, ${blue})`;
-};
-
-export const incrementScore = () => {
-  score.textContent = `${+(score?.textContent as string) + 10}`;
-};
-
-type TMoveKey = {
-  key: string;
-};
-
-export const moveKey = ({ key }: TMoveKey) => {
-  if (key == 'ArrowUp' && direction != 'down') {
-    setDirection('up');
-  }
-  if (key == 'ArrowDown' && direction != 'up') {
-    setDirection('down');
-  }
-  if (key == 'ArrowRight' && direction != 'left') {
-    setDirection('right');
-  }
-  if (key == 'ArrowLeft' && direction != 'right') {
-    setDirection('left');
-  }
-};
-
-export const chackEat = () => {
-  const head = snake[snake.length - 1];
-
-  if (head.x == food.x && head.y == food.y) {
-    incrementScore();
-    snake.push(head);
-    audio.play();
-
-    let x = randomPosition();
-    let y = randomPosition();
-
-    while (snake.find((position) => position.x == x && position.y == y)) {
-      x = randomPosition();
-      y = randomPosition();
-    }
-
-    food.x = x;
-    food.y = y;
-    food.color = randomColor();
-  }
-};
-
-export const checkCollision = () => {
-  const head = snake[snake.length - 1]; // posição da cabeça
-  const canvasLimit = canvas.width - size; // limite que a snake pode ir
-  const neckIndex = snake.length - 2; // posição do pescoço
-
-  const wallCollision = // bateu na perede?
-    head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit;
-
-  const selfCollision = snake.find((position, idx) => {
-    // bateu em si mesma? se sim retorna um objeto que sera convertido em boolean no if
-    return idx < neckIndex && position.x == head.x && position.y == head.y;
-  });
-
-  if (wallCollision || selfCollision) {
-    gameOver();
-  }
-};
-
-export const gameOver = () => {
-  setDirection(''); // parar a movimentação
-  menu.style.display = 'flex'; // mostra o menu
-  finalScore.textContent = score.textContent; // add o score atual ao score final
-  canvas.style.filter = 'blur(2px)'; // desfoca a o fundo do game
-
-  document.removeEventListener('keydown', moveKey);
 };
